f7011e5bcf03de4d046bb4983145e2135cf874d9
The Solin <solinsolinov@gmail.com>
2022-08-28 17:18:53 +0200

    Удаляет ненужные .css файлы
    Удаляет ненужные .css файлы
    Стили для принтера

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require('gulp')
-const { src,dest } = require('gulp')
+const { src,dest,parallel,series } = require('gulp')
 const htmlmin = require('gulp-htmlmin')
 const minifyCss = require('gulp-clean-css')
+const del = require('del')
 
 
 // Html
@@ -21,9 +22,50 @@ exports.html = html
 // Styles
 
 const styles = () => {
-    return src('dist/styles/**/*.css')
+    return src([
+        'dist/styles/**/*.css',
+        '!dist/styles/print.css'
+    ])
         .pipe(minifyCss())
-        .pipe(dest('dist/styles/'))
+        .pipe(dest('dist/styles'))
 }
 
 exports.styles = styles
+
+
+// Styles print
+
+const stylesPrint = () => {
+    return src('src/styles/print.css')
+        .pipe(minifyCss())
+        .pipe(dest('dist/styles'))
+}
+
+exports.stylesPrint = stylesPrint
+
+// Clean
+
+const clean = () => {
+    return del([
+        'dist/styles/blocks',
+        'dist/styles/**/*.css',
+        '!dist/styles/{styles, print}.css'
+    ])
+}
+
+exports.clean = clean
+
+
+// Default
+
+
+exports.default = gulp.series(
+    gulp.parallel(
+        html,
+        styles
+    ),
+    gulp.parallel(
+        clean,
+        stylesPrint
+    )
+)
--- src/scripts/modules/menu.js
+++ src/scripts/modules/menu.js
@@ -1,13 +1,13 @@
 (() => {
-    const menuButton = document.querySelector('.menu__button')
-    const menuList = document.querySelector('.menu__list')
-    const headerMenu = document.querySelector('.header__menu')
+    const menuButton = document.querySelector('.menu__button');
+    const menuList = document.querySelector('.menu__list');
+    const headerMenu = document.querySelector('.header__menu');
 
     menuButton.addEventListener('click', () => {
-        let expanded = menuButton.getAttribute('aria-expanded') === 'true'
-        menuButton.setAttribute('aria-expanded', !expanded)
-        menuButton.classList.toggle('menu__button--open')
-        menuList.classList.toggle('menu__list--open')
-        headerMenu.classList.toggle('menu__shadow')
-    })
-})()
+        let expanded = menuButton.getAttribute('aria-expanded') === 'true';
+        menuButton.setAttribute('aria-expanded', !expanded);
+        menuButton.classList.toggle('menu__button--open');
+        menuList.classList.toggle('menu__list--open');
+        headerMenu.classList.toggle('menu__shadow');
+    });
+})();
