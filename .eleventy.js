ca7a2cc84a11854995f0da215ed165076c294090
Solin Official <solinsolinov@gmail.com>
2022-08-31 20:49:26 +0200

    Изменение даты

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,15 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/scripts")
     eleventyConfig.addPassthroughCopy("src/articles/**/*.!(md)")
     eleventyConfig.addPassthroughCopy("src/favicon.svg")
+    eleventyConfig.addPassthroughCopy("src/robots.txt")
+
+    eleventyConfig.addFilter('ruDate', (value) => {
+        return value.toLocaleString('ru', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).replace(' г.', '')
+    })
 
     return {
         dir: {
